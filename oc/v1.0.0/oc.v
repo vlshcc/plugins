2d1606649f299f56f61979ddd404f2b45211f3b7
sarmonsiill <sarmonsiill@tilde.guru>
2026-04-15 17:26:03 +0200

    Added first draft of oc plugin

---
+++ oc/v1.0.0/oc.v
@@ -0,0 +1,48 @@
+module main
+
+import os
+
+fn main() {
+	op := if os.args.len > 1 { os.args[1] } else { '' }
+
+	match op {
+		'capabilities' {
+			println('command oc')
+			println('help')
+		}
+		'help' {
+			cmd := if os.args.len > 2 { os.args[2] } else { '' }
+			match cmd {
+				'oc', '' {
+					println('oc - wrapper for opencode (vlsh plugin)')
+					println('')
+					println('Usage:')
+					println('  oc <args...>   Translates to: opencode run "<args as single string>"')
+					println('')
+					println('Example:')
+					println('  oc write a small blog')
+					println('  # Becomes: opencode run "write a small blog"')
+				}
+				else {}
+			}
+		}
+		'run' {
+			cmd := if os.args.len > 2 { os.args[2] } else { '' }
+			match cmd {
+				'oc' {
+					if os.args.len > 3 {
+						args := os.args[3..]
+						combined := args.join(' ')
+						opencode_cmd := 'opencode run "${combined}"'
+						os.system(opencode_cmd)
+					} else {
+						os.system('opencode')
+					}
+					exit(os.exit_code())
+				}
+				else {}
+			}
+		}
+		else {}
+	}
+}
